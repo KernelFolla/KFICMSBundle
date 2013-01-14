f1202da22841519f4c86698f3a79eb60ad59746b
Marino <kernelfolla@gmail.com>
2013-01-14 09:40:26 +0100

    changed field names

---
+++ Entity/PostCategoriesCollection.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Marino
+ * Date: 10/01/13
+ * Time: 12.34
+ * To change this template use File | Settings | File Templates.
+ */ 
+class PostCategoriesCollection {
+
+}
